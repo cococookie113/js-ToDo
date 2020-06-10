9d2b0c56eb184269c1bdf8ad9211bec510641161
cococookie113 <cococookie113@gmail.com>
2020-06-10 19:59:57 +0900

    menu basic

--- menu.js
+++ menu.js
@@ -1,6 +1,17 @@
 const menuWhite = document.querySelector(".js-menu__white");
 const menuMain = document.querySelector(".js-menu");
 const menuLeft = document.querySelector("#menu-left");
+const pageItem = document.querySelectorAll(".menu__page-item");
+const aboutPage = document.getElementById("menu__about-page");
+const soundPage = document.getElementById("menu__sound-page");
+const imagePage = document.getElementById("menu__image-page");
+
+function showPage(pageName) {
+  pageItem.forEach(function (page) {
+    page.style.display = "none";
+  });
+  pageName.style.display = "block";
+}
 
 function showMenu() {
   menuMain.style.display = "flex";
@@ -13,6 +24,7 @@ function closeMenu() {
 function init() {
   menuWhite.addEventListener("click", showMenu);
   menuLeft.addEventListener("click", closeMenu);
+  showPage(aboutPage);
 }
 
 init();
